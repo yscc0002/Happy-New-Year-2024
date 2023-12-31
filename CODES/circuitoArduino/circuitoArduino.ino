72e386038ca11f2549489c19ca3f26f23d9503d8
Yoel Castillo <148872601+yscc0002@users.noreply.github.com>
2023-12-31 00:48:36 -0400

    Add files via upload

---
+++ CODES/circuitoArduino/circuitoArduino.ino
@@ -0,0 +1,112 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// PROTOTIPO DE FUNCIONES
+
+int cuantosLedAEncender(float numericProgress);
+void playMelody();
+
+// MELODIA
+
+#define NOTE_C6 1047
+#define NOTE_D6 1175
+#define NOTE_E6 1319
+#define NOTE_G6 1568
+#define NOTE_A6 1760
+#define NOTE_REST 0
+
+int melody[] = {
+  NOTE_E6, NOTE_D6, NOTE_C6, NOTE_D6,
+  NOTE_E6, NOTE_D6, NOTE_C6, NOTE_D6,
+  NOTE_G6, NOTE_E6, NOTE_A6, NOTE_A6,
+  NOTE_G6, NOTE_E6, NOTE_D6, NOTE_D6,
+  NOTE_C6, NOTE_E6, NOTE_G6, NOTE_G6,
+  NOTE_A6, NOTE_A6, NOTE_A6, NOTE_REST
+};
+
+int noteDurations[] = {
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4
+};
+
+// CARACTER CUADRADO COMPLETO PARA GRAFICA DEL PROGRESO EN LCD
+byte solidBox[8] = {
+  B11111111,
+  B11111111,
+  B11111111,
+  B11111111,
+  B11111111,
+  B11111111,
+  B11111111,
+  B11111111,
+};
+
+  double numericProgress = 0.0;
+
+void setup(){
+  lcd.createChar(0, solidBox);
+  lcd.begin(16,2);
+  Serial.begin(9600);
+}
+
+void loop(){
+
+  if (Serial.available()){
+    String mensajeRecibido = Serial.readStringUntil('\n');
+    numericProgress = mensajeRecibido.toDouble();
+
+  }
+
+  int graphicProgress = cuantosLedAEncender(numericProgress);
+
+  lcd.home();
+  lcd.print(numericProgress,15);
+  lcd.setCursor(0,1);
+  for (int x = 1; x<=graphicProgress;x++){
+      lcd.write(byte(0));
+    }
+  
+  while(numericProgress >= 100.0){
+    lcd.clear();
+    lcd.print(" ");
+    lcd.print("Happy New Year");
+    lcd.print(" ");
+    lcd.setCursor(0,1);
+    lcd.print("   ");
+    lcd.print("2024 Yoel!");
+    lcd.print("   ");
+    playMelody();
+    delay(86400000); // NO MORE PLAYMELODY
+    
+  }
+
+}
+
+// FUNCIONES
+
+void playMelody(){// NO HICE ESTA FUNCION NI LA DECLARACIONES DE LAS NOTAS
+  for (int i = 0; i < sizeof(melody) / sizeof(int); i++){
+    // note duration
+    int noteDuration = 250 / noteDurations[i];
+    // play the note
+    tone(9, melody[i], noteDuration);
+    // delay between notes
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the actual tone
+    noTone(9);
+  }
+}
+
+int cuantosLedAEncender(double numericProgress){//DISTRIBUCION
+
+  int vasAEncender = (16*numericProgress)/100;
+
+  return vasAEncender;
+}
+
---
+++ CODES/scriptPythonParaCalculoNumericoYEnvioASerie.py
@@ -0,0 +1,32 @@
+import serial
+import time
+from datetime import datetime
+
+def esBisiesto(ano):
+    if (ano % 4 == 0 and ano % 100 != 0) or (ano % 100 == 0 and ano % 400 == 0):
+        return True
+    else:
+        return False
+
+# HABLA CON ARDUINO
+arduino = serial.Serial('COM4', 9600)
+time.sleep(2)  # POR SI ACASO
+
+while True:
+
+    # TIMESTAMP DEL INICIO DEL AÑO 2023
+    hoy = datetime.now()
+    inicio2023 = datetime(2023, 1, 1)
+    tiempoPasadoEnSegundos = hoy.timestamp() - inicio2023.timestamp()
+
+    # CUANTOS SEGUNDOS TIENE EL ANO ACTUAL
+    segundosTotalesDelAno = 31622400 if esBisiesto(hoy.year) else 31536000
+
+    # CALCULA EL PROGRESO NUMERICO
+    progresoNumerico = (tiempoPasadoEnSegundos/segundosTotalesDelAno) * 100
+
+    # PREPARANDO Y ENVIANDO MENSAJE PARA EL PUERTO DEL ARDUINO (STRING)
+    mensaje = f"{progresoNumerico}\n"
+    arduino.write(mensaje.encode())
+
+    time.sleep(2)
